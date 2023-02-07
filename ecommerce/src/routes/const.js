11c0fc942215297a742a489c3b1d560a70a61563
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-07 20:15:20 +0200

    set Cart and Login routes

--- ecommerce/src/routes/const.js
+++ ecommerce/src/routes/const.js
@@ -1,11 +1,15 @@
 import MainLayout from "../layouts/MainLayout";
+import Cart from "../pages/Cart/Cart";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 import Product from "../pages/Product/Product";
 import Products from "../pages/Products/Products";
 
 export const HOME_PATH = "/";
-export const PRODUCTS_LIST_PATH = `${HOME_PATH}:category`;
+export const PRODUCTS_LIST_PATH = `${HOME_PATH}category/:category`;
 export const PRODUCT_PATH = `${PRODUCTS_LIST_PATH}/:productId`;
+export const CART = `${HOME_PATH}cart`;
+export const LOGIN = `${HOME_PATH}login`;
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
@@ -21,6 +25,14 @@ export const mainLayoutRoutes = {
     {
       path: PRODUCT_PATH,
       Component: Product,
+    },
+    {
+      path: CART,
+      Component: Cart,
+    },
+    {
+      path: LOGIN,
+      Component: Login,
     },
   ],
 };
