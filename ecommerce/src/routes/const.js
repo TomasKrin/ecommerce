fd382e7b9268557b1de9b90b784e7e64ea4638a2
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-09 20:25:21 +0200

    added some new paths

--- ecommerce/src/routes/const.js
+++ ecommerce/src/routes/const.js
@@ -1,5 +1,6 @@
 import MainLayout from "../layouts/MainLayout";
 import Cart from "../pages/Cart/Cart";
+import Checkout from "../pages/Checkout/Checkout";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Product from "../pages/Product/Product";
@@ -9,9 +10,10 @@ import Register from "../pages/Register/Register";
 export const HOME_PATH = "/";
 export const PRODUCTS_LIST_PATH = `${HOME_PATH}category/:category`;
 export const PRODUCT_PATH = `${PRODUCTS_LIST_PATH}/:productId`;
-export const CART = `${HOME_PATH}cart`;
-export const LOGIN = `${HOME_PATH}login`;
-export const REGISTER = `${HOME_PATH}register`;
+export const CART_PATH = `${HOME_PATH}cart`;
+export const LOGIN_PATH = `${HOME_PATH}login`;
+export const REGISTER_PATH = `${HOME_PATH}register`;
+export const CHECKOUT_PATH = `${HOME_PATH}checkout`;
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
@@ -29,16 +31,20 @@ export const mainLayoutRoutes = {
       Component: Product,
     },
     {
-      path: CART,
+      path: CART_PATH,
       Component: Cart,
     },
     {
-      path: LOGIN,
+      path: LOGIN_PATH,
       Component: Login,
     },
     {
-      path: REGISTER,
+      path: REGISTER_PATH,
       Component: Register,
+    },
+    {
+      path: CHECKOUT_PATH,
+      Component: Checkout,
     },
   ],
 };
